63b613825adf79022b9590c3b04c008880e77ef7
Lean <nosarazhi@gmail.com>
2023-12-03 15:46:55 -0300

    'Holav2'

--- frontend/src/componente/formulario.jsx
+++ frontend/src/componente/formulario.jsx
@@ -1,89 +1,111 @@
-// Formulario.jsx
-import React, { useState } from 'react';
-import { useEffect } from 'react';
+import { useState } from 'react';
 import './formularioo.css';
-import { getAllTasks } from '../api/formulario.api';
-
-
-
-async function cargarTurnos(){
-    const res = await getAllTasks();
-	console.log(res);
-}
-cargarTurnos();
 
 const Formulario = () => {
-    const [formularioData, setFormularioData] = useState({
-      nombre: '',
-      apellido: '',
-      fecha: '',
-      hora: '',
-    });
-  
-    const handleChange = (e) => {
-      const { name, value } = e.target;
-      setFormularioData({ ...formularioData, [name]: value });
-    };
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      console.log('Datos del formulario:', formularioData);
-    };
-  
-    return (
-      <div className="turnos">
-        <form onSubmit={handleSubmit} className="form-box">
-          <div className="form-group">
-            <label>
-              Nombre:
-              <input
-                type="text"
-                name="nombre"
-                value={formularioData.nombre}
-                onChange={handleChange}
-              />
-            </label>
-          </div>
-          <div className="form-group">
-            <label>
-              Apellido:
-              <input
-                type="text"
-                name="apellido"
-                value={formularioData.apellido}
-                onChange={handleChange}
-              />
-            </label>
-          </div>
-          <div className="form-group">
-            <label>
-              Fecha:
-              <input
-                type="date"
-                name="fecha"
-                value={formularioData.fecha}
-                onChange={handleChange}
-              />
-            </label>
-          </div>
-          <div className="form-group">
-            <label>
-              Hora:
-              <input
-                type="time"
-                name="hora"
-                value={formularioData.hora}
-                onChange={handleChange}
-              />
-            </label>
-          </div>
-          <div className="form-group">
-            <button type="submit">Enviar</button>
-          </div>
-        </form>
-      </div>
-    );
+  const [formularioData, setFormularioData] = useState({
+    especialidad: '',
+    servicio: '',
+    profesional: '',
+    fecha: '',
+    hora: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormularioData({ ...formularioData, [name]: value });
   };
-  
-  export default Formulario;
-  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Datos del formulario:', formularioData);
+    // Aquí puedes realizar acciones adicionales, como enviar los datos a la API.
+  };
+
+  return (
+    <div className="turnos">
+      <form onSubmit={handleSubmit} className="form-box">
+        <div className="form-group">
+          <label>
+            Especialidad:
+            <select
+              name="especialidad"
+              value={formularioData.especialidad}
+              onChange={handleChange}
+            >
+              <option value="">Selecciona una especialidad</option>
+              <option value="Barberia">Barberia</option>
+              <option value="Teñido de pelo">Teñido de pelo</option>
+              <option value="Masaje">Masaje</option>
+            </select>
+          </label>
+        </div>
+        <div className="form-group">
+          <label>
+            Servicio:
+            <select
+              name="servicio"
+              value={formularioData.servicio}
+              onChange={handleChange}
+            >
+              <option value="">Selecciona un servicio</option>
+              {formularioData.especialidad === 'Barberia' && (
+                <option value="Corte de pelo">Corte de pelo</option>
+              )}
+              {formularioData.especialidad === 'Teñido de pelo' && (
+                <option value="Teñido de pelo">Teñido de pelo</option>
+              )}
+              {formularioData.especialidad === 'Masaje' && (
+                <>
+                  <option value="Masaje cuerpo completo">Masaje cuerpo completo</option>
+                  <option value="Masaje Capilar">Masaje Capilar</option>
+                </>
+              )}
+            </select>
+          </label>
+        </div>
+        <div className="form-group">
+          <label>
+            Profesional:
+            <select
+              name="profesional"
+              value={formularioData.profesional}
+              onChange={handleChange}
+            >
+              <option value="">Selecciona un profesional</option>
+              <option value="Pepe">Pepe</option>
+              <option value="Juan">Juan</option>
+              <option value="Alberto">Alberto</option>
+            </select>
+          </label>
+        </div>
+        <div className="form-group">
+          <label>
+            Fecha:
+            <input
+              type="date"
+              name="fecha"
+              value={formularioData.fecha}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label>
+            Hora:
+            <input
+              type="time"
+              name="hora"
+              value={formularioData.hora}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <button type="submit">Enviar</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Formulario;
